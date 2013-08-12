5eb8a273ec30906e1417ec33fff76a597ad8feac
eltonsantos <eltonaxl@hotmail.com>
2013-08-12 19:39:37 -0300

    Improved all layout default. Create a view and controller home for test. Create a login page and fix layout. For now, it's ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rbootstrap::Application.routes.draw do
+
+  root :to => "home#index"
+
   devise_for :users
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
