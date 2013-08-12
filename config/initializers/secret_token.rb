5132d0edcc08783503cb3d243c605083215ce4ab
Sikora <sikora@ubuntu.(none)>
2013-08-12 17:31:36 -0300

    initial repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rbootstrap::Application.config.secret_key_base = '5467b6024bb04895ec94dd9c3b17740460337e28f0b75b58cd6f14073d794b476ac10602cace5e1265a76b79d46a088ee65a733254a869e8b43c1cdcc410cff2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rbootstrap::Application.config.session_store :cookie_store, key: '_rbootstrap_session'
